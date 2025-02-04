3fcc969e5776cbdd0d6f9a76635147caee5e2102
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2025-02-04 17:03:40 +0000

    part a

---
+++ PartA.py
@@ -0,0 +1,19 @@
+#Eoin O'Kennedy
+#30/01/2024
+#A Program to Print Steps 1 to 9 to the Screen
+
+
+
+def printSteps():
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+
+    
+print("Step 1")
+print("Step 2")
+print("Step 3")
+printSteps()
+print("Step 7")
+print("Step 8")
+print("Step 9")
